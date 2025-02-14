7348594c42df127c6515ae4cf7a5143f15ecf400
Andrei Mihai <andreimihai2305is@gmail.com>
2025-02-14 13:48:20 +0200

    made visualization functional

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+#define FPS 60
 
 #define SQUARE_SIZE 40
 #define WIDTH (1280 / SQUARE_SIZE)
@@ -94,81 +95,6 @@ void trace_route(List closed_list, Vector2 result[], Vector2 start)
     }
 }
 
-void a_star(Vector2 start, Vector2 end)
-{
-    const Cell start_cell = { .parent = start, .pos = start };
-    List open_list = {0};
-    List closed_list = {0};
-    Vector2 result[WIDTH*HEIGHT];
-    
-    append(&open_list, start_cell);
-
-    while(open_list.len != 0)
-    {
-        Cell q = open_list.items[0];
-        for (size_t i = 1; i < open_list.len; ++i)
-        {   
-            Cell it = open_list.items[i]; 
-            if (it.f < q.f) q = it;    
-        }
-        pop(&open_list, q.parent, q.f);
-
-        for (size_t i = 0; i < n_count; ++i)
-        {
-            Cell child = { 
-                .parent = q.pos,
-                .pos = { .x = q.pos.x + n_coords[i].x, .y = q.pos.y + n_coords[i].y },
-            };
-
-            if (invalid_pos(child.pos)) continue;
-
-            if (is_dest(child.pos, end)) 
-            {
-                found_dest = true;
-                break;
-            }
-
-            child.h = get_dist(child.pos, end);
-            child.g = q.f;
-            child.f = child.g + child.h;
-
-            bool found = false; 
-            for (size_t j = 0; j < open_list.len; j++)
-            {
-                Cell it = open_list.items[j];
-                if (it.pos.x == child.pos.x && it.pos.y == child.pos.y && it.f < child.f)
-                {
-                    found = true;
-                    break;
-                }
-            }
-
-            for (size_t j = 0; j < closed_list.len; j++)
-            {
-                Cell it = closed_list.items[j];
-                if (it.pos.x == child.pos.x && it.pos.y == child.pos.y && it.f < child.f)
-                {
-                    found = true;
-                    break;
-                }
-
-            }
-            if (found) continue;
-
-            append(&open_list, child);
-
-        }
-        append(&closed_list, q);
-        for (size_t i = 0; i < closed_list.len; ++i)
-        {
-            const Vector2 it = closed_list.items[i].pos;
-            draw_cell(it.y, it.x, GREEN);
-        }
-        if (found_dest) break;
-    }
-    trace_route(closed_list, result, start);
-}
-
 void draw_cell(int i, int j, Color color)
 {
     DrawRectangleV((Vector2) { j * SQUARE_SIZE, i * SQUARE_SIZE }, (Vector2) { SQUARE_SIZE, SQUARE_SIZE }, color);
@@ -212,28 +138,106 @@ void draw_grid()
     }
 }
 
-void draw_solution(void)
-{
-}
-
 int main(void)
 {
     grid_at(grid, 1, 1) = 2;
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "A* Visualizer");
    
-    Vector2 start = { 0, 0 };
-    Vector2 end = {HEIGHT -1 , WIDTH -1};
+    const Vector2 start = { 5, 30};
+    const Vector2 end = {16, 1};
+    const Cell start_cell = { .parent = start, .pos = start };
 
-    SetTargetFPS(10);
+    List open_list = {0};
+    List closed_list = {0};
+    Vector2 result[WIDTH*HEIGHT];
+    
+    append(&open_list, start_cell);
+
+    SetTargetFPS(FPS);
     while(!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_R)) 
+        {
+            open_list.len = 0;
+            closed_list.len = 0;
+            append(&open_list, start_cell);
+            found_dest = false;
+            for (size_t i = 0; i < WIDTH*HEIGHT; ++i) if (grid[i] == 1) grid[i] = 0;
+        }
+
         BeginDrawing();
-            ClearBackground(BLACK);
-            draw_cell(start.y, start.x, BLUE);
-            if (!found_dest) a_star(start, end);
-            draw_solution();
-            draw_cell(end.x, end.y, BLUE);
-            draw_grid();
+
+        ClearBackground(BLACK);            
+        if (!found_dest && open_list.len != 0)
+        {
+            Cell q = open_list.items[0];
+            for (size_t i = 1; i < open_list.len; ++i)
+            {   
+                Cell it = open_list.items[i]; 
+                if (it.f < q.f) q = it;    
+            }
+            pop(&open_list, q.parent, q.f);
+
+            for (size_t i = 0; i < n_count; ++i)
+            {
+                Cell child = { 
+                    .parent = q.pos,
+                    .pos = { .x = q.pos.x + n_coords[i].x, .y = q.pos.y + n_coords[i].y },
+                };
+
+                if (invalid_pos(child.pos)) continue;
+
+                if (is_dest(child.pos, end)) 
+                {
+                    found_dest = true;
+                    break;
+                }
+
+                child.h = get_dist(child.pos, end);
+                child.g = q.f;
+                child.f = child.g + child.h;
+
+                bool found = false; 
+                for (size_t j = 0; j < open_list.len; j++)
+                {
+                    Cell it = open_list.items[j];
+                    if (it.pos.x == child.pos.x && it.pos.y == child.pos.y && it.f < child.f)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                for (size_t j = 0; j < closed_list.len; j++)
+                {
+                    Cell it = closed_list.items[j];
+                    if (it.pos.x == child.pos.x && it.pos.y == child.pos.y && it.f < child.f)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (found) continue;
+
+                append(&open_list, child);
+
+            }
+            append(&closed_list, q);
+            for (size_t i = 0; i < closed_list.len; ++i)
+            {
+                const Vector2 it = closed_list.items[i].pos;
+                draw_cell(it.x, it.y, GREEN);
+            }
+        }
+        else
+        {
+            trace_route(closed_list, result, start);
+        }
+
+        draw_cell(start.x, start.y, BLUE);
+        draw_cell(end.x, end.y, BLUE);
+        draw_grid();
         EndDrawing();
     }
     CloseWindow();
